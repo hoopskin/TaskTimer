e472f8e5f21b2750d3df22c6101289f0f63fcb52
hoopskin <hoopskin@gmail.com>
2014-11-04 14:31:27 -0600

    Added getTask(id)

--- src/com/sorenson/tasktimer/sqlite/SQLiteHelper.java
+++ src/com/sorenson/tasktimer/sqlite/SQLiteHelper.java
@@ -4,6 +4,7 @@ import com.sorenson.tasktimer.model.Task;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -85,7 +86,7 @@ public class SQLiteHelper extends SQLiteOpenHelper {
 		values.put(TASKS_KEY_ID, task.getId());
 		values.put(TASKS_KEY_NAME, task.getName());
 		values.put(TASKS_KEY_GOAL_TIME_SECONDS, task.getGoalTimeSeconds());
-		
+
 		if(task.isReduce()) {
 			values.put(TASKS_KEY_REDUCE, 1);
 		} else {
@@ -96,8 +97,43 @@ public class SQLiteHelper extends SQLiteOpenHelper {
         db.insert(TABLE_TASKS, // table
         		null, //nullColumnHack
         		values); // key/value -> keys = column names/ values = column values
-        
+
         // 4. close
         db.close();
     }
+
+	public Task getTask(int id) {
+		// 1. get reference to readable DB
+		SQLiteDatabase db = this.getReadableDatabase();
+
+		// 2. build query
+        Cursor cursor =
+        		db.query(TABLE_TASKS, // a. table
+        		TASKS_COLUMNS, // b. column names
+        		" id = ?", // c. selections
+                new String[] { String.valueOf(id) }, // d. selections args
+                null, // e. group by
+                null, // f. having
+                null, // g. order by
+                null); // h. limit
+
+        // 3. if we got results get the first one
+        if (cursor != null) {
+			cursor.moveToFirst();
+		}
+
+		Task task = new Task();
+		task.setId(Integer.parseInt(cursor.getString(0)));
+		task.setName(cursor.getString(1));
+		task.setGoalTimeSeconds(Integer.parseInt(cursor.getString(2)));
+		if (Integer.parseInt(cursor.getString(3)) == 1) {
+			task.setReduce(true);
+		} else {
+			task.setReduce(false);
+		}
+
+		Log.d(TAG, "getTask("+id+") = " + task.toString());
+
+		return task;
+	}
 }
